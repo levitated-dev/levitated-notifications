2961b8619ee2a30daf44635aff80ff65c2afe730
Jan Puziewicz <jan.puziewicz@cargomedia.ch>
2014-07-24 12:25:59 +0200

    default config update

--- src/config/config.php
+++ src/config/config.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    'emailFrom' => 'MCT <jan@levitated.pl>',
+    // fill with a verified SES sender
+    'emailFrom' => 'name <email>',
 
     // number of sending tried before giving up.
     'maxTriesNum' => 8,
     'retryIn' => ['+1 seconds', '+2 seconds', '+3 seconds', '+10 seconds', '+1 minute'],
 
-    'sendOnlyToWhitelist' => false,
+    'sendOnlyToWhitelist' => true,
 
     // if set to true the whole process of sending will work normally except nothing will be actually sent
     // do NOT set this to true in production unless you know what you're doing: everything will look normal

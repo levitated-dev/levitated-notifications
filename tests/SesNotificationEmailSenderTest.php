3b81f46ed819394e1657aa01938fecdf03f79b60
Jan Puziewicz <jan@levitated.pl>
2014-09-20 15:15:36 +0200

    cleanup, formatting

--- tests/SesNotificationEmailSenderTest.php
+++ tests/SesNotificationEmailSenderTest.php
@@ -78,41 +78,4 @@ class SesNotificationEmailSenderTest extends TestCase {
         } catch (\Exception $e) {
         }
     }
-
-
-    //
-    //    public function testSend() {
-    //        // todo with laravel queue
-    //        return;
-    //        $queue = $this->getMockBuilder('Levitated\Notifications\NotificationQueueInterface')
-    //            ->getMock();
-    //        $queue->expects($this->once())
-    //            ->method('queueEmail');
-    //        $queue->expects($this->exactly(2))
-    //            ->method('queueSms');
-    //
-    //        $renderer = $this->getMockRenderer();
-    //        $n = new Notification(['emails' => ['foo@example.com'], 'phones' => ['123 123 123', '456 456 456']], 'bar', [], $renderer);
-    //        $n->send();
-    //    }
-    //
-    //    public function testSetRecipients() {
-    //        $renderer = $this->getMockRenderer();
-    //
-    //        $n = new Notification([], 'bar', [], $renderer);
-    //        $n->setRecipients(['emails' => ['foo@example.com'], 'phones' => ['123 123 123', '456 456 456']]);
-    //        $recipients = $n->getRecipients();
-    //        $this->assertCount(2, $recipients);
-    //        $this->assertArrayHasKey('emails', $recipients);
-    //        $this->assertContains('foo@example.com', $recipients['emails']);
-    //        $this->assertArrayHasKey('phones', $recipients);
-    //        $this->assertContains('123 123 123', $recipients['phones']);
-    //        $this->assertContains('456 456 456', $recipients['phones']);
-    //
-    //        $n->setRecipients(['emails' => 'foo@example.com', 'phones' => ['']]);
-    //        $recipients = $n->getRecipients();
-    //        $this->assertCount(1, $recipients);
-    //        $this->assertArrayHasKey('emails', $recipients);
-    //        $this->assertContains('foo@example.com', $recipients['emails']);
-    //    }
 }
